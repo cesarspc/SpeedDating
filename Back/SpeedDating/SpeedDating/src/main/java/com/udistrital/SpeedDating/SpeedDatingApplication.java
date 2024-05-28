ef0d393110199dd27a1f5ae463a44b732c559c2c
cesarSPC <149351724+cesarSPC@users.noreply.github.com>
2024-05-28 18:22:47 -0500

    Initial setup

---
+++ Back/SpeedDating/SpeedDating/src/main/java/com/udistrital/SpeedDating/SpeedDatingApplication.java
@@ -0,0 +1,13 @@
+package com.udistrital.SpeedDating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedDatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedDatingApplication.class, args);
+	}
+
+}
