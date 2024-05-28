89df0d053f935b1bbf51735c7130c17e5469ba82
cesarSPC <149351724+cesarSPC@users.noreply.github.com>
2024-05-28 18:22:47 -0500

    Initial setup

---
+++ Back/SpeedDating/SpeedDating/src/test/java/com/udistrital/SpeedDating/SpeedDatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udistrital.SpeedDating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedDatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
