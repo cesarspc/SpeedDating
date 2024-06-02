63750b4a6e617fcf1070db870a56f5b413b14686
cesarSPC <149351724+cesarSPC@users.noreply.github.com>
2024-06-02 15:48:03 -0500

    Endpoints standardization

--- Back/SpeedDating/SpeedDating/src/main/java/com/udistrital/SpeedDating/controladores/BuscadorController.java
+++ Back/SpeedDating/SpeedDating/src/main/java/com/udistrital/SpeedDating/controladores/BuscadorController.java
@@ -25,65 +25,64 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class BuscadorController {
-    
+
     BuscadorRepository database;
 
-    public BuscadorController(BuscadorRepository bd){
-        
+    public BuscadorController(BuscadorRepository bd) {
+
         this.database = bd;
     }
 
-@GetMapping("/api/crearBuscadores")
-public void crearBuscadores() {
-    /*
+    @GetMapping("/api/crearBuscadores")
+    public void crearBuscadores() {
+        /*
     Ejemplo introducción de datos
-    */
-    Buscador buscador1 = new Buscador();
-    buscador1.setNombre("Nombre1");
-    buscador1.setApellido("Apellido1");
-    buscador1.setEdad("25");
-    buscador1.setEstatura("1.70");
-    buscador1.setProfesion("Profesion1");
-    buscador1.setContextura("Contextura1");
-    buscador1.setEstadoCivil("Soltero");
-    buscador1.setIdentidadGenero("IdentidadGenero1");
-    buscador1.setCorreo("correo1@example.com");
-    buscador1.setTelefono("3121234567");
-    buscador1.setGustoContextura("Contextura2");
-    buscador1.setGustoInteres("Interes1");
-    buscador1.setGustoEstatura("1.80");
-    buscador1.setGustoIdentidad("IdentidadGenero2");
-    buscador1.setGustoEdad("30");
-    buscador1.setPagoHecho(false);
+         */
+        Buscador buscador1 = new Buscador();
+        buscador1.setNombre("Nombre1");
+        buscador1.setApellido("Apellido1");
+        buscador1.setEdad("25");
+        buscador1.setEstatura("1.70");
+        buscador1.setProfesion("Profesion1");
+        buscador1.setContextura("Contextura1");
+        buscador1.setEstadoCivil("Soltero");
+        buscador1.setIdentidadGenero("IdentidadGenero1");
+        buscador1.setCorreo("correo1@example.com");
+        buscador1.setTelefono("3121234567");
+        buscador1.setGustoContextura("Contextura2");
+        buscador1.setGustoInteres("Interes1");
+        buscador1.setGustoEstatura("1.80");
+        buscador1.setGustoIdentidad("IdentidadGenero2");
+        buscador1.setGustoEdad("30");
+        buscador1.setPagoHecho(false);
 
-    Buscador buscador2 = new Buscador();
-    buscador2.setNombre("Nombre2");
-    buscador2.setApellido("Apellido2");
-    buscador2.setEdad("30");
-    buscador2.setEstatura("1.80");
-    buscador2.setProfesion("Profesion2");
-    buscador2.setContextura("Contextura2");
-    buscador2.setEstadoCivil("Soltero");
-    buscador2.setIdentidadGenero("IdentidadGenero1");
-    buscador2.setCorreo("correo2@example.com");
-    buscador2.setTelefono("3121234568");
-    buscador2.setGustoContextura("Contextura1");
-    buscador2.setGustoInteres("Interes2");
-    buscador2.setGustoEstatura("1.70");
-    buscador2.setGustoIdentidad("IdentidadGenero2");
-    buscador2.setGustoEdad("25");
-    buscador2.setPagoHecho(true);
+        Buscador buscador2 = new Buscador();
+        buscador2.setNombre("Nombre2");
+        buscador2.setApellido("Apellido2");
+        buscador2.setEdad("30");
+        buscador2.setEstatura("1.80");
+        buscador2.setProfesion("Profesion2");
+        buscador2.setContextura("Contextura2");
+        buscador2.setEstadoCivil("Soltero");
+        buscador2.setIdentidadGenero("IdentidadGenero1");
+        buscador2.setCorreo("correo2@example.com");
+        buscador2.setTelefono("3121234568");
+        buscador2.setGustoContextura("Contextura1");
+        buscador2.setGustoInteres("Interes2");
+        buscador2.setGustoEstatura("1.70");
+        buscador2.setGustoIdentidad("IdentidadGenero2");
+        buscador2.setGustoEdad("25");
+        buscador2.setPagoHecho(true);
 
-    database.save(buscador1);
-    database.save(buscador2);
-}  
-    
-    
+        database.save(buscador1);
+        database.save(buscador2);
+    }
+
     @GetMapping("/api/buscadores")
-    public List<Buscador> obtenerBuscadores(){
+    public List<Buscador> obtenerBuscadores() {
         return database.findAll();
     }
-    
+
     @GetMapping("/api/buscadores/{id}")
     public ResponseEntity<Buscador> obtenerBuscadoresById(@PathVariable Long id) {
         Optional<Buscador> optional = database.findById(id);
@@ -95,40 +94,35 @@ public void crearBuscadores() {
         }
 
     }
-    
-    
-    @PostMapping("/api/buscador")
-    public ResponseEntity<Buscador> guardarBuscador(@RequestBody Buscador buscador){
+
+    @PostMapping("/api/buscadores")
+    public ResponseEntity<Buscador> guardarBuscador(@RequestBody Buscador buscador) {
         if (buscador.getId() != null) {
             return ResponseEntity.badRequest().build();
         }
-        
-        database.save(buscador);
-        return ResponseEntity.ok(buscador);
-    }
-    
-    
-    @PutMapping("/api/buscador")
-    public ResponseEntity<Buscador> actualizarBuscador(@RequestBody Buscador buscador){
-        if (buscador.getId() == null || !database.existsById(buscador.getId())) {
-            return ResponseEntity.badRequest().build();
-        }
-        
+
         database.save(buscador);
         return ResponseEntity.ok(buscador);
     }
 
-    @DeleteMapping("/api/buscador")
-    public ResponseEntity<Buscador> eliminarBuscador(@RequestBody Buscador buscador){
+    @PutMapping("/api/buscadores")
+    public ResponseEntity<Buscador> actualizarBuscador(@RequestBody Buscador buscador) {
         if (buscador.getId() == null || !database.existsById(buscador.getId())) {
             return ResponseEntity.badRequest().build();
         }
-        
+
+        database.save(buscador);
+        return ResponseEntity.ok(buscador);
+    }
+
+    @DeleteMapping("/api/buscadores")
+    public ResponseEntity<Buscador> eliminarBuscador(@RequestBody Buscador buscador) {
+        if (buscador.getId() == null || !database.existsById(buscador.getId())) {
+            return ResponseEntity.badRequest().build();
+        }
+
         database.delete(buscador);
         return ResponseEntity.noContent().build();
-    }    
-    
-    
-    
-    
+    }
+
 }
