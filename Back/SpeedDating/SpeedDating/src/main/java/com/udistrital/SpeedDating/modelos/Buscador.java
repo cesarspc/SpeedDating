2ec8fd997792f71bd86f12f185bb87bfc7da1887
Juan Peralta <guayaba.dev@gmail.com>
2024-06-01 22:54:13 -0500

    Clases de postulante y Buscador creados

--- Back/SpeedDating/SpeedDating/src/main/java/com/udistrital/SpeedDating/modelos/Buscador.java
+++ Back/SpeedDating/SpeedDating/src/main/java/com/udistrital/SpeedDating/modelos/Buscador.java
@@ -4,10 +4,76 @@
  */
 package com.udistrital.SpeedDating.modelos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 /**
  *
  * @author cesar
  */
+@Data
+@Entity
+@RequiredArgsConstructor
 public class Buscador {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NonNull
+    private String nombre;
+    
+    @NonNull
+    private String apellido;
+    
+    @NonNull
+    private String edad;
+    
+    @NonNull
+    private String estatura;
+    
+    @NonNull
+    private String profesion;
+    
+    @NonNull
+    private String contextura;
+    
+    @NonNull
+    private String estadoCivil;
+    
+    @NonNull
+    private String identidadGenero;
+    
+    @NonNull
+    private String correo;
+    
+    @NonNull
+    private String telefono;
+
+    @NonNull
+    private String gustoContextura;
+    
+    @NonNull
+    private String gustoInteres;
+    
+    @NonNull
+    private String gustoEstatura;
+    
+    @NonNull
+    private String gustoIdentidad;
+    
+    @NonNull
+    private String gustoEdad;
+
+    private boolean pagoHecho;
+    
+    public Buscador() {
+        super();
+    }
+       
 }
--- Back/SpeedDating/SpeedDating/src/main/java/com/udistrital/SpeedDating/modelos/Postulante.java
+++ Back/SpeedDating/SpeedDating/src/main/java/com/udistrital/SpeedDating/modelos/Postulante.java
@@ -4,10 +4,66 @@
  */
 package com.udistrital.SpeedDating.modelos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 /**
  *
  * @author cesar
  */
+@Data
+@Entity
+@RequiredArgsConstructor
 public class Postulante {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NonNull
+    private String nombre;
+    
+    @NonNull
+    private String apellido;
+    
+    @NonNull
+    private String edad;
+    
+    @NonNull
+    private String estatura;
+    
+    @NonNull
+    private String profesion;
+    
+    @NonNull
+    private String contextura;
+    
+    @NonNull
+    private String estadoCivil;
+    
+    @NonNull
+    private String identidadGenero;
+    
+    @NonNull
+    private String correo;
+    
+    @NonNull
+    private String telefono;
+
+    @NonNull
+    private String interesPrincipal;
+    
+    @NonNull
+    private String diponibilidad;
+     
+    
+    public Postulante() {
+        super();
+    }
+       
 }
